2d0ce5ec6a75f7e80b6a51a570d00c4b8069733e
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-09-14 17:44:23 +0700

    add: dependency mapstruct

--- src/main/java/com/jobportal/api/mapper/UserMapper.java
+++ src/main/java/com/jobportal/api/mapper/UserMapper.java
@@ -1,19 +1,20 @@
 package com.jobportal.api.mapper;
 
 import com.jobportal.api.dto.user.UserDTO;
+import com.jobportal.api.model.user.Role;
 import com.jobportal.api.model.user.User;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
-@Component
-public class UserMapper {
+@Mapper(componentModel = "spring")
+public interface UserMapper {
 
-    public UserDTO mapUserToUserDTO(User user) {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setId(user.getId());
-        userDTO.setEmail(user.getEmail());
-        userDTO.setActive(user.isActive());
-        userDTO.setRegistrationDate(user.getRegistrationDate());
-        userDTO.setRole(user.getRole().getName());
-        return userDTO;
+    @Mapping(source = "role", target = "role", qualifiedByName = "mapRoleToString")
+    UserDTO mapUserToUserDTO(User user);
+
+    @Named("mapRoleToString")
+    default String mapRoleToString(Role role) {
+        return role != null ? role.getName() : null;
     }
 }

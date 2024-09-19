8fba9ad42c3372c49e01acc584aab7f3d2ed7f4f
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-09-19 22:24:30 +0700

    refactor: exception

--- src/main/java/com/jobportal/api/exception/CustomException.java
+++ src/main/java/com/jobportal/api/exception/CustomException.java
@@ -1,10 +1,8 @@
 package com.jobportal.api.exception;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class CustomException extends RuntimeException {
 
     private final EnumException enumException;
--- src/main/java/com/jobportal/api/exception/EnumException.java
+++ src/main/java/com/jobportal/api/exception/EnumException.java
@@ -9,14 +9,14 @@ public enum EnumException {
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "Invalid request data"),
     USER_EXISTED(HttpStatus.BAD_REQUEST, "User already existed"),
 
-    // 401 : Unauthorized — user chưa được xác thực và truy cập vào resource yêu cầu phải xác thực
+    // 401 : Unauthorized — user chưa được xác thực
     UNAUTHENTICATED(HttpStatus.UNAUTHORIZED, "You are not authenticated"),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid token"),
     INVALID_OTP(HttpStatus.UNAUTHORIZED, "Invalid OTP or OTP expired"),
     INVALID_LOGIN(HttpStatus.UNAUTHORIZED, "Invalid email or password"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Current password is incorrect"),
 
-    // 403: Forbidden — user không có quyền truy cập vào resource
+    // 403: Forbidden — user không có quyền
     UNAUTHORIZED(HttpStatus.FORBIDDEN, "You do not permission"),
     ADMIN_CANNOT_BE_DELETED(HttpStatus.FORBIDDEN, "Cannot delete user with ADMIN role"),
 
@@ -25,16 +25,16 @@ public enum EnumException {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found"),
     PROFILE_NOT_FOUND(HttpStatus.NOT_FOUND, "Profile not found"),
 
-    // 500: Internal Server Error — có lỗi xẩy ra trong hệ thống
+    // 500: Internal Server Error — có lỗi trong hệ thống
     UNCATEGORIZED_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "Uncategorized Exception: "),
     JWT_SIGNING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to sign the JWT token"),
     ;
+
+    private final HttpStatus statusCode;
+    private final String message;
 
     EnumException(HttpStatus statusCode, String message) {
         this.statusCode = statusCode;
         this.message = message;
     }
-
-    private final HttpStatus statusCode;
-    private final String message;
 }

dedacba64c700e164f6d90749f08e7d8c05c068a
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-11-18 15:17:06 +0700

    update: Enum Exception

--- src/main/java/com/jobportal/api/exception/EnumException.java
+++ src/main/java/com/jobportal/api/exception/EnumException.java
@@ -6,32 +6,32 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum EnumException {
     // 400 : Bad request — dữ liệu gửi lên không hợp lệ
-    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "Invalid request data"),
-    INVALID_FILE_NAME(HttpStatus.BAD_REQUEST, "Invalid file name"),
-    USER_EXISTED(HttpStatus.BAD_REQUEST, "User already existed"),
-    COMPANY_EXISTED(HttpStatus.BAD_REQUEST, "Company already existed"),
-    JOB_POST_NOT_FOUND(HttpStatus.BAD_REQUEST, "Job post not found"),
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "Dữ liệu yêu cầu không hợp lệ"),
+    INVALID_FILE_NAME(HttpStatus.BAD_REQUEST, "Tên file không hợp lệ"),
+    USER_EXISTED(HttpStatus.BAD_REQUEST, "Người dùng đã tồn tại"),
+    COMPANY_EXISTED(HttpStatus.BAD_REQUEST, "Công ty đã tồn tại"),
+    JOB_POST_NOT_FOUND(HttpStatus.BAD_REQUEST, "Không tìm thấy công việc"),
 
     // 401 : Unauthorized — user chưa được xác thực
-    UNAUTHENTICATED(HttpStatus.UNAUTHORIZED, "You are not authenticated"),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid token"),
-    INVALID_OTP(HttpStatus.UNAUTHORIZED, "Invalid OTP or OTP expired"),
-    INVALID_LOGIN(HttpStatus.UNAUTHORIZED, "Invalid email or password"),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Current password is incorrect"),
+    UNAUTHENTICATED(HttpStatus.UNAUTHORIZED, "Chưa xác thực"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Token không hợp lệ"),
+    INVALID_OTP(HttpStatus.UNAUTHORIZED, "OTP không hợp lệ"),
+    INVALID_LOGIN(HttpStatus.UNAUTHORIZED, "Email hoặc mật khẩu không đúng"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Mật khẩu hiện tại không đúng"),
 
     // 403: Forbidden — user không có quyền
-    UNAUTHORIZED(HttpStatus.FORBIDDEN, "You do not have permission"),
-    ADMIN_CANNOT_BE_DELETED(HttpStatus.FORBIDDEN, "Cannot delete user with ADMIN role"),
+    UNAUTHORIZED(HttpStatus.FORBIDDEN, "Không có quyền truy cập"),
+    ADMIN_CANNOT_BE_DELETED(HttpStatus.FORBIDDEN, "Không thể xóa tài khoản admin"),
 
     // 404: Not found — không tồn tại resource
-    PAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "Page not found"),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found"),
-    PROFILE_NOT_FOUND(HttpStatus.NOT_FOUND, "Profile not found"),
+    PAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "Không tìm thấy trang"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "Người dùng không tồn tại"),
+    PROFILE_NOT_FOUND(HttpStatus.NOT_FOUND, "Hồ sơ không tồn tại"),
 
     // 500: Internal Server Error — có lỗi trong hệ thống
-    UNCATEGORIZED_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "Uncategorized Exception: "),
-    JWT_SIGNING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to sign the JWT token"),
-    ERROR_UPLOAD_FILE(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to upload file"),
+    UNCATEGORIZED_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "Ngoại lệ chưa phân loại: "),
+    JWT_SIGNING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Không thể ký JWT Token"),
+    ERROR_UPLOAD_FILE(HttpStatus.INTERNAL_SERVER_ERROR, "Lỗi khi tải file"),
     ;
 
     private final HttpStatus statusCode;
